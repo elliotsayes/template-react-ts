42fb9f8826a1c40bc782f96a5ef4031460f4a5d7
Elliot Sayes <elliot@sayes.cloud>
2024-05-25 09:03:51 +0900

    Loading tilemap

--- src/game/scenes/TmjStatic.ts
+++ src/game/scenes/TmjStatic.ts
@@ -5,6 +5,7 @@ export class TmjStatic extends Scene
 {
     camera: Phaser.Cameras.Scene2D.Camera;
     background: Phaser.GameObjects.Image;
+    tilemap: Phaser.Tilemaps.Tilemap;
     tmjStaticText : Phaser.GameObjects.Text;
 
     constructor ()
@@ -12,19 +13,44 @@ export class TmjStatic extends Scene
         super('TmjStatic');
     }
 
+    preload ()
+    {
+        this.load.image('Primary', 'assets/tileset/Primary.png');
+        this.load.tilemapTiledJSON('Tilemap', 'assets/tileset/Tilemap.json')
+    }
+
     create ()
     {
         this.camera = this.cameras.main
         this.camera.setBackgroundColor(0xff0000);
 
-        this.background = this.add.image(512, 384, 'background');
-        this.background.setAlpha(0.5);
+        // this.background = this.add.image(512, 384, 'Primary');
+        
+        this.tilemap = this.make.tilemap({ key: 'Tilemap' })
+        const tileset = this.tilemap.addTilesetImage(
+            'Primary', // tileset name
+            'Primary', // key of the tileset image
+        )!
 
-        this.tmjStaticText = this.add.text(512, 384, 'TmjStatic', {
+        // const layer0 = this.tilemap.createLayer("Background0", tileset, 0, 0)
+        // const layer1 = this.tilemap.createLayer("Background1", tileset, 0, 0)
+        // console.log(this.tilemap, layer0, layer1)
+
+        this.tilemap.layers.filter((layer) => layer.name.startsWith('BG_')).forEach((layer) => {
+            this.tilemap.createLayer(layer.name, tileset, 0, 0)
+        })
+
+        this.tilemap.layers.filter((layer) => layer.name.startsWith('FG_')).forEach((layer) => {
+            this.tilemap.createLayer(layer.name, tileset, 0, 0)
+        })
+
+        this.camera.centerOn(this.tilemap.widthInPixels/2, this.tilemap.heightInPixels/2)
+
+        this.tmjStaticText = this.add.text(512, 0, 'TmjStatic', {
             fontFamily: 'Arial Black', fontSize: 64, color: '#ffffff',
             stroke: '#000000', strokeThickness: 8,
             align: 'center'
-        }).setOrigin(0.5).setDepth(100);
+        }).setOrigin(0.5, 0).setDepth(100);
         
         EventBus.emit('current-scene-ready', this);
     }
